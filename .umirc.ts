c927ce376d498d0f163c82bb5b2004cfa4b235f6
多牧 <xuzhi.mxz@antfin.com>
2020-12-30 16:21:55 +0800

    doc: git doc build

--- .umirc.ts
+++ .umirc.ts
@@ -5,10 +5,12 @@ export default defineConfig({
   favicon:
     'https://gw.alipayobjects.com/zos/antfincdn/cfg5jFqgVt/DiceGraph.png',
   logo: 'https://gw.alipayobjects.com/zos/antfincdn/cfg5jFqgVt/DiceGraph.png',
-  outputPath: 'docs-dist',
+  outputPath: 'docs',
   locales: [
     ['zh-CN', '中文'],
     ['en-US', 'English'],
   ],
+  resolve: { includes: ['doc', 'src'] },
+  publicPath: './'
   // more config: https://d.umijs.org/config
 });
